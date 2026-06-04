71c06c389a9a3073a9d99068fd9a88755aab14f4
MAREDDY SATHVIKA <mareddysathvika02@gmail.com>
2026-06-04 22:34:14 +0530

    Update Quiz_main.py

--- Quiz_main.py
+++ Quiz_main.py
@@ -1,6 +1,6 @@
-from data import question_data
-from question_model import Question
-from quiz_brain import QuizBrain
+from Quiz_data import question_data
+from Quiz_question_model import Question
+from Quiz_quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
